a0409546dc78860dea21e50b83093cfc81e6ebf1
AstudilloUMA <pastudillof02@uma.es>
2024-04-03 09:38:32 +0200

    New methods of doubleLinkedList

--- Practica2/src/main/java/coladoble/DoubleLinkedList.java
+++ Practica2/src/main/java/coladoble/DoubleLinkedList.java
@@ -4,6 +4,8 @@ AUTORES: Manuel Jesús Jerez Sánchez y Pablo Astudillo Fraga
 
 package coladoble;
 
+import java.util.Comparator;
+
 public class DoubleLinkedList<T> implements DoubleLinkedQueue<T> {
 
     private LinkedNode<T> first;
@@ -95,5 +97,81 @@ public class DoubleLinkedList<T> implements DoubleLinkedQueue<T> {
     @Override
     public int size() {
         return size;
+    }
+
+    @Override
+    public T get(int index) {
+        if(index < 0)
+            throw new DoubleLinkedQueueException("No hay indice negativo");
+        else {
+            int i = 0;
+            LinkedNode<T> node = first;
+
+            while (i < index) {
+                node = node.getNext();
+                index++;
+            }
+
+            return node.getItem();
+        }
+    }
+
+    @Override
+    public boolean contains(T value) {
+        LinkedNode<T> node = first;
+
+        while(node != null && !node.getItem().equals(value))
+            node = node.getNext();
+
+        return node != null;
+    }
+
+    @Override
+    public void remove(T value) {
+        if(!contains(value))
+            throw new DoubleLinkedQueueException("No existe este valor en la cola doble");
+        else {
+            LinkedNode<T> prev = null;
+            LinkedNode<T> node = first;
+
+            while (!node.getItem().equals(value)) {
+                prev = node;
+                node = node.getNext();
+            }
+
+            if (node.isFirstNode()) {
+                node.getNext().setPrevious(null);
+                first = node.getNext();
+            } else if (node.isLastNode()) {
+                prev.setNext(null);
+                last = prev;
+            } else {
+                node.getNext().setPrevious(prev);
+                prev.setNext(node.getNext());
+            }
+        }
+    }
+
+    @Override
+    public void sort(Comparator<? super T> comparator) {
+        if (size <= 1) return;
+
+        LinkedNode<T> node = first;
+
+        while (node != null) {
+            LinkedNode<T> minNode = node;
+            LinkedNode<T> aux = node.getNext();
+            while (aux != null) {
+                if (comparator.compare(aux.getItem(), minNode.getItem()) < 0) {
+                    minNode = aux;
+                }
+                aux = aux.getNext();
+            }
+
+            T auxItem = node.getItem();
+            node.setItem(minNode.getItem());
+            minNode.setItem(auxItem);
+            node = node.getNext();
+        }
     }
 }
