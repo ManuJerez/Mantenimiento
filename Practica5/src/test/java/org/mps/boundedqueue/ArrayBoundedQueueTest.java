25c0ada92b664d7b5650d7bb23d744a73fbfc8a6
Manu Jerez <manujs66@gmail.com>
2024-05-02 17:25:11 +0200

    Ultimos cambios y retoques P5

--- Practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
+++ Practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -1,20 +1,25 @@
+/*
+AUTORES:
+- MANUEL JESUS JEREZ SANCHEZ
+- PABLO ASTUDILLO FRAGA
+ */
+
 package org.mps.boundedqueue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperties;
 
 import java.util.Iterator;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ArrayBoundedQueueTest {
-    ArrayBoundedQueue boundedQueue;
+    ArrayBoundedQueue<Integer> boundedQueue;
 
     @Nested
-    @DisplayName("Con cola vacia")
+    @DisplayName("Empty queue")
     class emptyBoundedQueue{
         @BeforeEach
         void setupEmptyQueue(){
@@ -22,7 +27,7 @@ public class ArrayBoundedQueueTest {
         }
 
         @Test
-        @DisplayName("Crear cola con capacidad nula lanza excepcion")
+        @DisplayName("Create empty queue throw exception")
         void create_NullCapacityBoundedQueue_ThrowException(){
             Throwable createNullCapacityQueue = catchThrowable( () -> {
                 new ArrayBoundedQueue<>(0);
@@ -32,52 +37,75 @@ public class ArrayBoundedQueueTest {
         }
 
         @Test
-        @DisplayName("Put inserta un nuevo elemento en la cola")
+        @DisplayName("Put insert an element in queue")
         void put_InsertAnElementInQueue(){
             boundedQueue.put(4);
             assertThat(boundedQueue).contains(4);
         }
 
         @Test
-        @DisplayName("Get lanza excepcion")
-        void get_ReturnAnElementOfTheQueue(){
+        @DisplayName("Put a null element throw exception")
+        void put_NullElement_ThrowException(){
+            Throwable putNullElement = catchThrowable( () -> {
+                boundedQueue.put(null);
+            });
+            assertThat(putNullElement).hasMessage("put: element cannot be null");
+        }
+
+        @Test
+        @DisplayName("Get throw exception")
+        void get_ThrowException(){
             assertThatThrownBy(boundedQueue::get).hasMessage("get: empty bounded queue");
         }
 
         @Test
-        @DisplayName("isFull devuelve false")
+        @DisplayName("isFull return false")
         void isFull_ReturnFalse(){
-            assertThat(boundedQueue.isFull()).isFalse();
+            boolean res = boundedQueue.isFull();
+            assertThat(res).isFalse();
         }
 
         @Test
-        @DisplayName("isEmpty devuelve true")
+        @DisplayName("isEmpty return true")
         void isEmpty_ReturnTrue(){
             assertThat(boundedQueue).isEmpty();
         }
 
         @Test
-        @DisplayName("Size == 0")
+        @DisplayName("Size is zero")
         void size_ReturnZero(){
-            assertThat(boundedQueue.size()).isZero();
+            int size = boundedQueue.size();
+            assertThat(size).isZero();
         }
 
         @Test
-        @DisplayName("Primer elemento es cero")
+        @DisplayName("First element is zero")
         void getFirst_ReturnZero(){
-            assertThat(boundedQueue.getFirst()).isZero();
+            int first = boundedQueue.getFirst();
+            assertThat(first).isZero();
         }
 
         @Test
-        @DisplayName("Ultimo elemento es cero")
+        @DisplayName("Last element is zero")
         void getLast_ReturnZero(){
-            assertThat(boundedQueue.getLast()).isZero();
+            int last = boundedQueue.getLast();
+            assertThat(last).isZero();
         }
 
         @Test
-        @DisplayName("Iterator devuelve iterador vacio y lanza excepcion")
+        @DisplayName("Last element after put is 1")
+        void getLast_ReturnOne(){
+            boundedQueue.put(2);
+
+            int last = boundedQueue.getLast();
+
+            assertThat(last).isOne();
+        }
+
+        @Test
+        @DisplayName("Iterate in empty iterator throw exception")
         void iterator_ReturnEmptyIterator(){
-            Iterator iterator = boundedQueue.iterator();
+            Iterator<Integer> iterator = boundedQueue.iterator();
             assertThat(iterator.hasNext()).isFalse();
             assertThatThrownBy(iterator::next).hasMessage("next: bounded queue iterator exhausted");
         }
@@ -85,10 +113,10 @@ public class ArrayBoundedQueueTest {
     }
 
     @Nested
-    @DisplayName("Con cola llena")
+    @DisplayName("Full queue")
     class fullBoundedQueue{
         @BeforeEach
-        void setupEmptyQueue(){
+        void setupFullQueue(){
             boundedQueue = new ArrayBoundedQueue<>(5);
             boundedQueue.put(1);
             boundedQueue.put(2);
@@ -98,11 +126,11 @@ public class ArrayBoundedQueueTest {
         }
 
         @Nested
-        @DisplayName("Put insert an element int the queue")
+        @DisplayName("Put insert an element in the queue")
         public class put_OnFullQueue
         {
             @Test
-            @DisplayName("put insert a null element in a full queue")
+            @DisplayName("Put a null element in full queue throw exception")
             void put_InsertANullElement_ThrowException()
             {
                 Throwable insertNullValue = catchThrowable( () -> {
@@ -113,7 +141,7 @@ public class ArrayBoundedQueueTest {
             }
 
             @Test
-            @DisplayName("put insert an element in a full queue")
+            @DisplayName("Put an element in full queue throw exception")
             void put_InsertAnElement_ThrowException()
             {
                 Throwable insert = catchThrowable( () -> {
@@ -128,7 +156,7 @@ public class ArrayBoundedQueueTest {
         @DisplayName("Get return the first element of the queue and delete the element from the queue")
         void get_ReturnAnElementOfTheQueue()
         {
-            int element = (int) boundedQueue.get();
+            int element = boundedQueue.get();
 
             assertThat(element).isEqualTo(1);
             assertThat(boundedQueue).doesNotContain(element);
@@ -153,10 +181,10 @@ public class ArrayBoundedQueueTest {
         }
 
         @Test
-        @DisplayName("size return the value of the queue length that is 5")
+        @DisplayName("Size return the value of the queue length that is 5")
         void size_ReturnQueueLength()
         {
-            int size = boundedQueue.size();
+            int size = 5;
 
             assertThat(boundedQueue).size().isEqualTo(size);
         }
@@ -183,7 +211,7 @@ public class ArrayBoundedQueueTest {
         @DisplayName("Iterator iterate the elements from a queue")
         public class iterator_OnAFullQueue
         {
-            Iterator iterator;
+            Iterator<Integer> iterator;
             @BeforeEach
             void setupIterator()
             {
@@ -200,12 +228,13 @@ public class ArrayBoundedQueueTest {
             }
 
             @Test
-            @DisplayName("next return the next element")
+            @DisplayName("Next return the next element")
             void next_ReturnNext()
             {
-                int next = (int) iterator.next();
+                int next = iterator.next();
+                int firstElement = boundedQueue.getFirst();
 
-                assertThat(boundedQueue).element(boundedQueue.getFirst()).isEqualTo(next);
+                assertThat(boundedQueue).element(firstElement).isEqualTo(next);
             }
         }
     }
