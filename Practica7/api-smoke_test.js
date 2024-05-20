db33ac3d3755a7ed38781f3892872d6ad4c5a984
Manu Jerez <manujs66@gmail.com>
2024-05-20 17:54:44 +0200

    Inicio Practica7

---
+++ Practica7/api-smoke_test.js
@@ -0,0 +1,25 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 5,
+    duration: '1m',
+    thresholds: {
+        http_req_failed: [{
+            threshold: 'rate <= 0',
+            abortOnFail: true
+        }],
+        http_req_duration: ['avg < 100'],
+    },
+};
+
+export default function () {
+    const url = "http://localhost:8080/medico/1";
+    const res = http.get(url);
+    console.log(res.body);
+
+    check(res, {
+        'response code was 200': (res) => res.status == 200,
+    });
+    sleep(1);
+}
